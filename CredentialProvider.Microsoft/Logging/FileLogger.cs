4be7eb205a2b9666286edee326c00f1d1b4e29ad
Laura VanLaningham <47076136+atlslscsrv-app@users.noreply.github.com>
2020-10-04 21:36:01 -0400

    Initialize users/zarenner/taskCanceled

---
+++ CredentialProvider.Microsoft/Logging/FileLogger.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+//
+// Licensed under the MIT license.
+
+using System;
+using System.IO;
+using NuGet.Common;
+
+namespace NuGetCredentialProvider.Logging
+{
+    internal class FileLogger : ILogger, IDisposable
+    {
+        Lazy<StreamWriter> lazyWriter;
+
+        internal FileLogger(string filePath)
+        {
+            lazyWriter = new Lazy<StreamWriter>(() => new StreamWriter(new FileStream(filePath, FileMode.Create, FileAccess.Write, FileShare.ReadWrite)));
+        }
+
+        public void SetLogLevel(LogLevel newLogLevel)
+        {
+            // If enabled, FileLogger always logs all messages for diagnostic purposes
+        }
+
+        public void Log(LogLevel logLevel, string message)
+        {
+            lazyWriter.Value.WriteLine($"[{logLevel}] {message}");
+        }
+
+        #region IDisposable Support
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing && lazyWriter.IsValueCreated)
+                {
+                    lazyWriter.Value.Dispose();
+                }
+
+                disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion
+    }
+}
---
+++ CredentialProvider.Microsoft/Logging/PluginConnectionLogger.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+//
+// Licensed under the MIT license.
+
+using System.Threading;
+using NuGet.Common;
+using NuGet.Protocol.Plugins;
+
+namespace NuGetCredentialProvider.Logging
+{
+    internal class PluginConnectionLogger : LoggerBase
+    {
+        private readonly IConnection connection;
+
+        internal PluginConnectionLogger(IConnection connection)
+        {
+            this.connection = connection;
+        }
+
+        protected override void WriteLog(LogLevel logLevel, string message)
+        {
+            // intentionally not awaiting here -- don't want to block forward progress just because we tried to log.
+            FireAndForgetLog(logLevel, message);
+        }
+
+        private async void FireAndForgetLog(LogLevel logLevel, string message)
+        {
+            try
+            {
+                await connection.SendRequestAndReceiveResponseAsync<LogRequest, LogResponse>(
+                    MessageMethod.Log,
+                    new LogRequest(logLevel, $"    {message}"),
+                    CancellationToken.None);
+            }
+            catch
+            {
+                // Intentionally empty.
+                // It's possible we'll fail to log if the plugin connection is in a bad state.
+                // Other loggers if attached will capture the actual error. This failure to log is likely not interesting enough to log to stderr.
+            }
+        }
+    }
+}
